29de1aa72c5ff32f98fcf223a41347da1d7d9693
Wadhah <wadhah.sky@gmail.com>
2023-12-26 21:12:49 +0300

    Add new job in 'ci.yml' file that related to deploy project resource to Linode cloud

--- deploy/setup-docker.sh
+++ deploy/setup-docker.sh
@@ -45,12 +45,32 @@ else
 
   install -m 0755 -d /etc/apt/keyrings
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  # Note: The GPG keys are used to securely exchange information and verify the identity of the sender. GPG keys are
+  #       often utilized in package management systems, email encryption, and software distribution in the Linux ecosystem
+  #       to ensure data security and integrity.
+  # Info: The GPG (GNU Privacy Guard) command in Linux is a powerful tool used for secure communication and data storage.
+  #       The option -y to gpg command will automatically answer "yes" to prompts in case there is already
+  #       '/etc/apt/keyrings/docker.gpg' file exists and will asking if you want to overwrite it or not.
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg -y --dearmor -o /etc/apt/keyrings/docker.gpg
 
   chmod a+r /etc/apt/keyrings/docker.gpg
 
   echo "Add the repository to (apt) package manager sources"
 
+  # The 'tee' command, used with a pipe, reads standard input, then writes the output of a program to standard output
+  # and simultaneously copies it into the specified file or files. Use the tee command to view your output immediately
+  # and at the same time, store it for future use.
+
+  # Note: if you want to check all the available releases of Docker engine to Ubuntu, check:
+  #
+  #       https://download.docker.com/linux/ubuntu
+
+  # Info: with following command i have faced the following error:
+  #
+  #       The repository 'https://download.docker.com/linux/ubuntu 'mantic' Release' does not have a Release file.
+  #
+  #       I found out the mantic (version of my ubuntu) shouldn't be in quotes as well as the system architecture value
+  #       within arch=<value>
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   apt-get update -y
@@ -63,9 +83,9 @@ else
 
   docker -v | grep -E "Docker version" || ehco "Docker is NOT installed successfully"
 
-  echo "Step 4: Starting Docker service..."
+  echo "Step 4: Starting Docker service, will take a minute..."
 
-  service docker start
+  sleep 60 && service docker start
 
   echo "Done! :)"
 
